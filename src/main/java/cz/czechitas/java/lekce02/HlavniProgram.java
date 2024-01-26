c25e12933c4891464a97aa7c9813cadee154bba4
Pavlina Sedlackova <p1sedlackova@gmail.com>
2024-01-26 19:51:02 +0100

    Nakreslení kompletního obrázku dle zadání.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -17,22 +17,51 @@ public class HlavniProgram {
         //TODO tady bude kód
         zofka.setPenColor(Color.decode("100011"));
 
-        for (int i = 0; i < 2; i++) {
-            novaPoziceDolu();
-            novaPoziceDoprava();
-        }
+        novaPoziceDolu();
+        novaPoziceDoprava();
         nakresliPrasatko();
 
-        for (int i = 0; i < 6; i++) {
-            novaPoziceNahoru();
-            novaPoziceDoleva();
-        }
+        novaPoziceSlunicko();
         nakresliSlunicko();
 
+/*
         for (int i = 0; i < 2; i++) {
             novaPoziceDoprava();
         }
-        nakresliOsmiUhelnik();
+         nakresliOsmiUhelnik();
+*/
+
+        novaPozicePrvniDomecek();
+        nakresliDomecek();
+
+        novaPoziceNahoru();
+
+        nakresliDomecek();
+
+        for (int i = 0; i < 4; i++) {
+            novaPoziceDoprava();
+            nakresliDomecek();
+        }
+
+       novaPozicePosledniDomecek();
+        nakresliDomecek();
+
+        zofka.penUp();
+        zofka.move(500);
+        zofka.turnLeft(90);
+        zofka.move(320);
+        zofka.turnRight(90);
+        zofka.penDown();
+
+        nakresliPismenoP();
+        novaPozicePismeno();
+        nakresliPismenoA();
+        novaPozicePismeno();
+        nakresliPismenoV();
+        novaPozicePismeno();
+        nakresliPismenoK();
+        novaPozicePismeno();
+        nakresliPismenoA();
     }
 
 
@@ -40,7 +69,8 @@ public class HlavniProgram {
         zofka.turnLeft(90);
 
         this.nakresliDomecek();
-        zofka.turnRight(35);
+        zofka.move(150);
+        zofka.turnLeft(55);
 
         this.nakresliNozicky();
         zofka.turnRight(125);
@@ -67,6 +97,9 @@ public class HlavniProgram {
         zofka.move(Math.sqrt(2) * 50);
         zofka.turnRight(135);
         zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnRight(180);
     }
 
     public void nakresliNozicky() {
@@ -92,14 +125,14 @@ public class HlavniProgram {
 
     public void novaPoziceNahoru() {
         zofka.penUp();
-        zofka.move(100);
+        zofka.move(230);
         zofka.penDown();
     }
 
     public void novaPoziceDoprava() {
         zofka.penUp();
         zofka.turnRight(90);
-        zofka.move(100);
+        zofka.move(150);
         zofka.turnLeft(90);
         zofka.penDown();
     }
@@ -107,7 +140,7 @@ public class HlavniProgram {
     public void novaPoziceDolu() {
         zofka.penUp();
         zofka.turnLeft(180);
-        zofka.move(200);
+        zofka.move(400);
         zofka.turnLeft(180);
         zofka.penDown();
     }
@@ -118,6 +151,36 @@ public class HlavniProgram {
         zofka.move(100);
         zofka.turnRight(90);
         zofka.penDown();
+    }
+
+    public void novaPoziceSlunicko() {
+        zofka.penUp();
+        for (int i = 0; i < 4; i++) {
+            this.novaPoziceDoleva();
+        }
+        zofka.penUp();
+        zofka.move(550);
+        zofka.penDown();
+
+
+    }
+    public void novaPozicePosledniDomecek () {
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(230);
+        zofka.turnLeft(180);
+        zofka.penDown();
+    }
+
+    public void novaPozicePrvniDomecek() {
+        this.novaPoziceDolu();
+        this.novaPoziceDoleva();
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(150);
+        zofka.turnRight(180);
+        zofka.penDown();
+        zofka.turnRight(5);
     }
 
     public void nakresliSlunicko() {
@@ -137,6 +200,85 @@ public class HlavniProgram {
         zofka.turnLeft(90);
     }
 
+    public void nakresliPismenoP() {
+        zofka.move(100);
+        zofka.turnRight(90);
+
+        for (int i = 0; i < 5; i++) {
+            zofka.move(10);
+            zofka.turnRight(90);
+            zofka.move(5);
+            zofka.turnLeft(90);
+        }
+        zofka.turnRight(90);
+        zofka.move(20);
+        zofka.turnRight(90);
+
+        for (int i = 0; i < 5; i++) {
+            zofka.move(10);
+            zofka.turnLeft(90);
+            zofka.move(5);
+            zofka.turnRight(90);
+        }
+        zofka.turnLeft(90);
+        zofka.move(50);
+        zofka.turnLeft(180);
+    }
+
+    public void nakresliPismenoA() {
+        zofka.turnRight(25);
+        zofka.move(110);
+        zofka.turnRight(135);
+        zofka.move(105);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnLeft(65);
+        zofka.move(35);
+        zofka.turnRight(180);
+        zofka.move(35);
+        zofka.turnRight(65);
+        zofka.move(60);
+        zofka.turnLeft(160);
+    }
+
+    public void nakresliPismenoV() {
+        zofka.turnLeft(25);
+        zofka.move(100);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnLeft(135);
+        zofka.move(100);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnRight(160);
+    }
+
+    public void nakresliPismenoK() {
+        zofka.move(100);
+        zofka.turnRight(180);
+        zofka.move(40);
+        zofka.turnLeft(40);
+        zofka.move(70);
+        zofka.turnLeft(180);
+        zofka.move(70);
+        zofka.turnRight(85);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+        zofka.move(60);
+        zofka.turnLeft(180);
+    }
+
+    public void novaPozicePismeno(){
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(55);
+        zofka.turnLeft(90);
+        zofka.penDown();
+    }
+
+    /*
     public void nakresliOsmiUhelnik() {
         for (int i = 0; i < 8; i++) {
             zofka.turnRight(45);
@@ -144,5 +286,5 @@ public class HlavniProgram {
         }
         zofka.turnLeft(5);
     }
-
+*/
 }
